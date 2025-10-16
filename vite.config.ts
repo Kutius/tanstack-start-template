e3f17863c0f87b076df5be1d4df7265c06f863d4
Kutius <precious.wei@qq.com>
2025-10-16 15:21:39 +0800

    chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,11 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 3344,
   },
   plugins: [
     tsConfigPaths(),
-    tanstackStart({ customViteReactPlugin: true }),
+    tanstackStart(),
     viteReact(),
     tailwindcss(),
   ],
